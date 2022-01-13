19ca6df65bb70d4bc92e5483946745f3fb17465d
Pablo Raviola <pablo.raviola@externos-ar.cencosud.com>
2022-01-13 09:26:43 -0300

    fixs

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,15 +9,15 @@ export default function AboutMe() {
         className="text-gray-300 text-sm sm:text-base"
         style={{ fontFamily: "Poppins" }}
       >
-        I'm a fullstack web developer and a systems engineering student from san
-        francisco, córdoba - arg my main goal right now is to get my engineering
-        degree, which will hopefully be next year. In the meantime, i'm building
-        my career as a developer learning as much as i can about the new
-        technologies that come across. I consider myself as a proactive person
-        who's always willing to take new challenges in order to improve his
-        skills. On my spare time, i like listening to music and playing drums, i
-        also love travelling whenever i have the chance, whether it is going on
-        a short trip or taking a journey abroad for a couple of months
+        I'm a fullstack web developer and a systems engineering student from San
+        Francisco, Córdoba - ARG. My main goal right now is to get my
+        engineering degree, which will hopefully be next year. In the meantime,
+        I'm building my career as a developer learning as much as I can about
+        the new technologies that come across. I consider myself as a proactive
+        person who's always willing to take new challenges in order to improve
+        his skills. On my spare time, I like listening to music and playing
+        drums, I also love travelling whenever I have the chance, whether it is
+        going on a short trip or taking a journey abroad for a couple of months
       </p>
       <div className="lg:hidden flex flex-col md:flex-row md:justify-center border-t-2 p-4 mt-3 space-y-2 md:space-x-5">
         <div className="flex items-center text-white space-x-2">
